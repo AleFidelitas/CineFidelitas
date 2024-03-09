13480f9b8772711519e36cd1b3ee7910d4735a85
rjimenez <rjimenez@COM-RAJM-WXI>
2024-03-09 10:41:19 -0600

    App CineFide, creacion del proyecto

---
+++ src/main/java/com/cinefide/CinefideApplication.java
@@ -0,0 +1,13 @@
+package com.cinefide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinefideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinefide/CinefideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinefide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinefideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
