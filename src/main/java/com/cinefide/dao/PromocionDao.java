d95129db0b07f27afe66c87f744dcec6f9046b61
bryandavila <123339631+bryandavila@users.noreply.github.com>
2024-03-09 13:19:22 -0600

    Proyecto CineFide, Promocion

---
+++ src/main/java/com/cinefide/dao/PromocionDao.java
@@ -0,0 +1,9 @@
+package com.cinefide.dao;
+
+import com.cinefide.domain.Promocion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromocionDao extends JpaRepository<Promocion,Long>{
+    
+}
+
